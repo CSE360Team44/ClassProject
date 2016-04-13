c7c9e549e833e295a36a44fcf6bb68f5fae738b0
aegonz12 <anthonyegonzalez@gmail.com>
2016-04-12 20:56:50 -0700

    Added files via upload

---
+++ Code/Menu.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+public class Menu 
+{
+	
+	public static void printMenu()
+	{
+		System.out.print("Menu\n" +
+				"1 - roll\n" +
+				"2 - new game\n" +
+				"3 - view stats\n" +
+				"4 - view rules\n" +
+				"5 - quit\n" +
+				"\n\n\n");
+	}
+	
+	public static void runMenu()
+	{
+		Scanner in = new Scanner(System.in);
+		int input = 0;
+		
+		
+		
+		while(input != 5)
+		{
+			printMenu();
+			
+			if(input == 1)
+			{
+				roll();
+			}
+			else if(input == 2)
+			{
+				newGame();
+			}
+			else if(input == 3)
+			{
+				viewStats();
+			}
+			else if(input ==4)
+			{
+				viewRules();
+			}
+			
+				
+			
+			
+			input = in.nextInt();
+			
+			
+		}
+		
+		System.out.println("goodbye");
+	}
+	
+	public static void roll()
+	{
+		System.out.println("roll");
+	}
+	
+	public static void newGame()
+	{
+		System.out.println("new game");
+	}
+	
+	public static void viewStats()
+	{
+		System.out.println("view stats");
+	}
+	
+	public static void viewRules()
+	{
+		System.out.println("view rules");
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		runMenu();
+		
+
+	}//main end
+
+}//class end
+
+
---
+++ Code/Player.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Player
+{
+	private int overallScore;
+	private int tempScore;
+    private String name;
+    private int diceLeft;
+    private boolean turnEnd;
+    
+
+    public Player(String name)
+    {
+        this.overallScore = 0;
+        this.tempScore = 0;
+        this.name = name;
+        this.diceLeft = 0;
+        this.turnEnd = false;
+    }
+    
+    public int getTempScore()
+    {
+        return this.tempScore;
+    }
+    
+    public int getOverallScore()
+    {
+    	return this.overallScore;
+    }
+    
+    public void setTempScore(int score)
+    {
+        this.tempScore = score;
+    }
+    
+    public void setOverallScore(int score)
+    {
+    	this.overallScore = score;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }   
+    
+    public int roll()
+    {  
+    	int myRolls[] = tempRoll();
+    	
+    	return computeScore(myRolls);
+    }
+    
+    public int[] tempRoll()
+    {
+    	Dice myDice = new Dice(diceLeft);
+    	
+    	return myDice.rollDice();
+    }
+    
+    //needs to update turnEnd and diceLeft
+    private int computeScore(int diceResult[])
+    {
+    	int[] count = new int[6];
+    	for(int index = 0; index < diceResult.length; index++)
+    		count[diceResult[index] - 1]++;
+    }
+    		
+    
+}
+
