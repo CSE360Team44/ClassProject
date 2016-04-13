f1de17142f5db8e08e580082b56d0f47c0cb07cb
aegonz12 <anthonyegonzalez@gmail.com>
2016-04-12 20:56:01 -0700

    Create Dice.java

---
+++ Code/Dice.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Dice
+{
+  private int minimum = 1;
+	private int maximum = 6;
+
+	private int numOfDice;
+	private int list[];
+
+	Dice(int numOfDice)
+	{
+		this.numOfDice =numOfDice;
+		list = new int[numOfDice];
+	}
+
+	public int[] rollDice()
+	{
+		for(int i=0;i < numOfDice;i++)
+			list[i] = roll();
+
+
+		return list;
+
+	}
+
+	private int roll()
+	{
+
+		Random rand = new Random();
+	
+		int value =( rand.nextInt((maximum - minimum) + 1) + minimum); 
+
+		return value;
+
+	}
+}
