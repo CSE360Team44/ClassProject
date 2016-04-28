64c6d690df8bf27f0514901fd7f4bb38605fbfcd
Patricia Evans <patricia.k.evans@gmail.com>
2016-04-27 20:48:10 -0700

    Update Game.java
    Updating comments

--- week3/Game.java
+++ week3/Game.java
@@ -4,6 +4,21 @@ import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * Class to implement game play. The game class creates a game with the number of player inputed,
+ * 		and input of the winning score. Players are created with a name and their scores 
+ * 		updated. Game class checks if the winning score has been reached. 
+ * 		Default initialization for private int winningScore, private int currentPlayer,
+ * 		private Player players[], and private String stats.
+ * 
+ * @author CSE360 Spring 2016 Team 44: Fernando Avalos,
+ * 		    		       Maria Castro,
+ * 		    	   	       Patricia Evans,
+ * 		    		       Anthony Gonzalez,
+ * 		    		       Ivan Soledad.
+ * @version April 27, 2016
+ * 
+ */
 public class Game{
 	
 	private int winningScore;
@@ -11,11 +26,14 @@ public class Game{
 	private Player players[];
 	private String stats;
 	
-	
 	/**
-	 * Constructor that creates a new Game
-	 * @param playersNum number of players in the game
-	 * @param winScore score needed to win the game
+	 * Game - Constructor initializes the Player array size based on the number of 
+	 * 		players input to play the game, the index of the current player to be used
+	 * 		in the array, the number of points inputted to win the game, and setting each 
+	 * 		player with a name. 
+	 * 				
+	 * @param playersNum int the number of players playing the game.
+	 * 	  winScore int the number of point required to win the game. 
 	 */
 	Game(int playersNum, int winScore)
 	{
@@ -26,8 +44,10 @@ public class Game{
 	}
 
 	/**
-	 * Sets the names of the players
-	 * @param array array of players
+	 * setNames - iterates through the player array inserting the
+	 * 		name entered by the user. 
+	 * 						
+	 * @param array Player[] contains a list of the player names in the game. 
 	 */
 	public void setNames(Player[] array)
 	{
@@ -48,10 +68,20 @@ public class Game{
 	}
 
 	/**
-	 * Function that implements roll
+	 * gameRoll - Current player rolls, the score is checked for winning score, 
+	 * 	           the winning score and player is stored in a textfile, and
+	 * 		   gameFinished will be set to true.
+	 * 		   If the player has not yet won and the turn has ended,
+	 * 		   the temp score is 0, the next player can roll, and 
+	 *  		   gameFinished remains false. 
+	 * 	
+	 * @throws IOException						
+	 * @param none
+	 * @return gameFinished boolean a flag that will become true when the game has finished. 
 	 */
 	public boolean gameRoll() throws IOException 
 	{
+		
 		boolean gameFinished = false;
 
 		// current player rolls
@@ -61,11 +91,11 @@ public class Game{
 		// if they have, the game is over
 		if(players[currentPlayer].checkWin(winningScore))
 		{
+			
 			storeResult(players[currentPlayer].getName());
 			System.out.println("Game over\n");
 			//this.gameOver();
 			gameFinished = true;
-			
 			
 		}
 
@@ -73,10 +103,11 @@ public class Game{
 		// once the turn ends, go to next player
 		if(players[currentPlayer].getTurnEnd() &&  players[currentPlayer].checkWin(winningScore) == false)
 		{
+			
 			players[currentPlayer].setTempScore(0);
 			nextPlayer();
-		}
 		
+		}
 		
 		System.out.print("your overall score is now : " + players[currentPlayer].getOverallScore() + 
 				"\nyour temp score is now " + players[currentPlayer].getTempScore() + "\nyour name is " + players[currentPlayer].getName()
@@ -84,22 +115,31 @@ public class Game{
 		
 		updateStats();
 		
-
 		return gameFinished;
+	
 	}
 
 	/**
-	* Gets the current player
-	* @return players[currentPlayer] the current player in the player array
+	* getCurrentPlayer - Returns the current player in the players array using the currentPlayer
+	* 			variable as the index.
+	* 
+	* @return players[currentPlayer] Player the current player contained in the players array
 	*/
 	public Player getCurrentPlayer()
 	{
+		
 		return players[currentPlayer];
+
 	}
 	
 	/**
-	 * Function that implements the hold function
-	 */
+	* hold - Returns a value of true if the player holds dice, and false if the player continues
+	* 			with their rolls. If the player holds, the score is recorded and the 
+	* 			player will have their turn. 
+	* 
+	* @return held boolean a flag that will become true if the player holds on their dice roll 
+	* 			and false if the player continues to roll. 
+	*/
 	public boolean hold()
 	{
 		// held = whether one has held
@@ -133,7 +173,12 @@ public class Game{
 		outputFile.close();
 	}
 	
-	// prints the players' names and scores
+	/**
+	 * printStats - iterates through the players array printing the name and
+	 * 		score of the user. 
+	 * 						
+	 * @param none
+	 */
 	private void printStats()
 	{
 		for(int index = 0; index < players.length; index++)
