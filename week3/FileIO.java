98cba1fc3598cdc0e9bc48953a3cbd2345316b28
Patricia Evans <patricia.k.evans@gmail.com>
2016-04-29 16:57:46 -0700

    Update FileIO.java
    Update comments.

--- week3/FileIO.java
+++ week3/FileIO.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.io.FileReader;
 import java.util.Scanner;
 
-
-
 /**
  * Class to read and write winning player names from each game played to a file.  
  * 
@@ -15,20 +13,20 @@ import java.util.Scanner;
  * 		    	   	       Patricia Evans,
  * 		    		       Anthony Gonzalez,
  * 		    		       Ivan Soledad.
- * @version April 27, 2016
+ * @version April 29, 2016
  * 
  */
 public class FileIO 
 {
 	
 	/**
+	 * readFile - Reads a file line by line and returns arraylist of contents
 	 * 
 	 * @throws FileNotFoundException						
-	 * @param fileName String
-	 * @return list ArrayList<String> 
+	 * @param fileName String contains the file name needed to be read
+	 * @return list ArrayList<String> contains the contents of the file, a list of winners. 
 	 */
 	//do not omit file extension for parameter, returns arraylist of file contents
-	// reads a file line by line and returns arraylist of contents
 	public static ArrayList<String> readFile(String fileName) throws FileNotFoundException
 	{
 		
@@ -49,16 +47,16 @@ public class FileIO
 		
 	}
 	
-	
 	/**
-	 *   
+	 * appendIntToFile - Method will write a winner's name to the file on a new line.
+	 * 			Will append the line to the file to keep track of game 
+	 * 			history. 
 	 * 	
 	 * @throws IOException						
-	 * @param fileName String
-	 * 	  lineToWrite String
+	 * @param fileName String contains the file name to write. 
+	 * 	  lineToWrite String contains the String that is to be written in the file. 
 	 */
-	//do not omit file extension for parameter, appends line to the file if doesnt exist it creates it
-	// wrte a line to a file
+	//do not omit file extension for parameter, appends line to the file if doesnt exist it creates it.
 	public static void appendIntToFile(String fileName, String lineToWrite) throws IOException
 	{
 		
@@ -71,15 +69,17 @@ public class FileIO
 	}
 	
 	/**
-	 * 
+	 * getHistory - Method to open the file of all past winners. Will add
+	 * 			the most recent winner on a new line to the file 
+	 * 			by appending the winner's name to String winners. 
 	 * 	
 	 * @throws FileNotFoundException						
 	 * @param none 
-	 * @return winners String
+	 * @return winners String contains the winners from all games played.
 	 */
-	//open file of past winners, return contents in string
 	public static String getHistory() throws FileNotFoundException
 	{
+		
 		String winners = "";
 		
 		ArrayList<String> arrayOfWinners =  FileIO.readFile("History.txt");
